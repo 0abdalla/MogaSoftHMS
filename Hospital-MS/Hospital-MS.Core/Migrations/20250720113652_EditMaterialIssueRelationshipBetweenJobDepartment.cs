0c29809343027767423f324718bb1b91a98f8bff
Eslam Eltayar <tayaruwk@gmail.com>
2025-07-20 18:07:07 +0300

    worked on treasury movements and transactions

---
+++ Hospital-MS/Hospital-MS.Core/Migrations/20250720113652_EditMaterialIssueRelationshipBetweenJobDepartment.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital_MS.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditMaterialIssueRelationshipBetweenJobDepartment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MaterialIssuePermissions_Branches_BranchId",
+                schema: "finance",
+                table: "MaterialIssuePermissions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MaterialIssuePermissions_BranchId",
+                schema: "finance",
+                table: "MaterialIssuePermissions");
+
+            migrationBuilder.DropColumn(
+                name: "BranchId",
+                schema: "finance",
+                table: "MaterialIssuePermissions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "JobDepartmentId",
+                schema: "finance",
+                table: "MaterialIssuePermissions",
+                type: "int",
+                nullable: true);
+
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaterialIssuePermissions_JobDepartmentId",
+                schema: "finance",
+                table: "MaterialIssuePermissions",
+                column: "JobDepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MaterialIssuePermissions_JobDepartment_JobDepartmentId",
+                schema: "finance",
+                table: "MaterialIssuePermissions",
+                column: "JobDepartmentId",
+                principalTable: "JobDepartment",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MaterialIssuePermissions_JobDepartment_JobDepartmentId",
+                schema: "finance",
+                table: "MaterialIssuePermissions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MaterialIssuePermissions_JobDepartmentId",
+                schema: "finance",
+                table: "MaterialIssuePermissions");
+
+            migrationBuilder.DropColumn(
+                name: "JobDepartmentId",
+                schema: "finance",
+                table: "MaterialIssuePermissions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BranchId",
+                schema: "finance",
+                table: "MaterialIssuePermissions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaterialIssuePermissions_BranchId",
+                schema: "finance",
+                table: "MaterialIssuePermissions",
+                column: "BranchId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MaterialIssuePermissions_Branches_BranchId",
+                schema: "finance",
+                table: "MaterialIssuePermissions",
+                column: "BranchId",
+                principalSchema: "finance",
+                principalTable: "Branches",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- Hospital-MS/Hospital-MS.Core/Models/MaterialIssuePermission.cs
+++ Hospital-MS/Hospital-MS.Core/Models/MaterialIssuePermission.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Hospital_MS.Core.Models.HR;
 
 namespace Hospital_MS.Core.Models;
 public class MaterialIssuePermission : AuditableEntity // اذن الصرف
@@ -13,13 +9,15 @@ public class MaterialIssuePermission : AuditableEntity // اذن الصرف
     public string? DocumentNumber { get; set; }
     public int StoreId { get; set; }
     public Store Store { get; set; }
-    public int BranchId { get; set; }
-    public Branch Branch { get; set; }
+    //public int BranchId { get; set; }
+    // public Branch Branch { get; set; }
     public string? Notes { get; set; }
     public bool IsActive { get; set; } = true;
 
+    public int? JobDepartmentId { get; set; }
+    public JobDepartment? JobDepartment { get; set; } = default!;
+
     public int? DisbursementRequestId { get; set; }
     public DisbursementRequest? DisbursementRequest { get; set; } = default!;
-
     public ICollection<MaterialIssueItem> Items { get; set; } = new List<MaterialIssueItem>();
 }
